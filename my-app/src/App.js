acdb49e944af26d2792cf0f158ee52f863cc8344
Didenko17 <vladislav.didenko.2001@gmail.com>
2020-03-01 21:12:36 +0300

    Maximum name length set. Added input of name and number.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import './App.css';
 import Card from './Card';
-function App() {
-  return (
-    <Card/>
-    );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      number:'',
+      date:[],
+      name:'',
+      cvv:'000',
+    };
+    this.nameChange=this.nameChange.bind(this);
+    this.numberChange=this.numberChange.bind(this);
+  }
+  componentDidMount(){
+    const temp = new Date();
+    const month=(temp.getMonth()+1<10)?'0'+(temp.getMonth()+1):temp.getMonth()+1;
+    this.setState({
+      date:[month,temp.getFullYear()-2000]
+    })
+  }
+  nameChange(e){
+    if(this.state.name.length<25||e.target.value.length<this.state.name.length){
+      this.setState({
+        name:e.target.value
+      });
+    }
+  }
+  numberChange(e){
+    if(this.state.number.length<16||e.target.value.length<this.state.number.length){
+      this.setState({
+        number:e.target.value
+      })
+    }
+  }
+  render(){
+    return (
+      <div className='App'>
+        <Card number={this.state.number} date={this.state.date} name={this.state.name?this.state.name:'Your Name'}/>
+        <form>
+          <label for="name">Card Holder</label>
+          <input id="name" type="text" value={this.state.name} onChange={this.nameChange}></input>
+          <label for="number">Card Number</label>
+          <input id="number" type="text" value={this.state.number} onChange={this.numberChange}></input>
+        </form>
+      </div>
+      );
+  }
 }
 
 export default App;
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 import visa from './visa.png'
 import mastercard from './mastercard.png'
 class Card extends React.Component {
     constructor(props){
         super(props);
-        this.state={
-            number:['4202','7237','4426','8461'],
-            date:['03','20'],
-            name:'Your Name',
-            cvv:'000',
-        };
     }
     checkPaymentSystem(){
-        const character=this.state.number[0].charAt(0);
+        const character=this.props.number.charAt(0);
         switch(character){
-            case '4':return visa;
-            case '5':return mastercard;
-            default: return '';
+          case '4':return visa;
+          case '5':return mastercard;
+          default: return '';
         }
     }
+    getNumber(number){
+        let result=number;
+        while(result.length<16){
+            result+='#';
+        }
+        return [result.slice(0,4),result.slice(4,8),result.slice(8,12),result.slice(12)];
+    }
     render(){
+        const number=this.getNumber(this.props.number);
+        console.log(number);
         return (
-        <div className="bank-card">
-            <div className='number'>
-                <span className='number-item'>{this.state.number[0]}</span>
-                <span className='number-item'>{this.state.number[1]}</span>
-                <span className='number-item'>{this.state.number[2]}</span>
-                <span className='number-item'>{this.state.number[3]}</span>
+            <div className="bank-card">
+                <div className='number'>
+                    <span className='number-item'>{number[0]}</span>
+                    <span className='number-item'>{number[1]}</span>
+                    <span className='number-item'>{number[2]}</span>
+                    <span className='number-item'>{number[3]}</span>
+                </div>
+            <div className='date'>{this.props.date.join('/')}</div>
+            <div className='name'>{this.props.name.toLocaleUpperCase()}</div>
+            <img className='payment-system' src={this.checkPaymentSystem()} alt=''></img>
             </div>
-        <div className='date'>{this.state.date.join('/')}</div>
-        <div className='name'>{this.state.name.toLocaleUpperCase()}</div>
-        <img className='payment-system' src={this.checkPaymentSystem()} alt='payment system logo'></img>
-        </div>
         );
     }
 }
